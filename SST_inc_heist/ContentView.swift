701764cf1628793c682bbe0d334ef55dbcb91bdd
a <>
2025-09-01 15:52:38 +0800

    no message

---
+++ SST_inc_heist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SST_inc_heist
+//
+//  Created by Wang Yuzhi Ryan on 1/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SST_inc_heist/SST_inc_heistApp.swift
@@ -0,0 +1,17 @@
+//
+//  SST_inc_heistApp.swift
+//  SST_inc_heist
+//
+//  Created by Wang Yuzhi Ryan on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SST_inc_heistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
